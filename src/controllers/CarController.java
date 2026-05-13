2d18703e0020e8e5df21d7bfaad4343bf277b03b
Your Name <you@example.com>
2026-05-13 13:42:34 +0000

    Ya acabe 3

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -6,20 +6,21 @@ public class CarController {
 
 
   public void sortByName(Car[] cars){
-    for (int i = 0; i < cars.length; i++) {
+    for (int i = 0; i < cars.length-1; i++) {
       boolean swap=false;
       for(int j=0;j<cars.length-1-i;j++){
-        if(cars[j].getName().compareTo(cars[i].getName())>0){
+        if(cars[j].getName().compareTo(cars[j+1].getName())>0){
           Car aux=cars[j];
           cars[j]=cars[j+1];
           cars[j+1]=aux;
           swap=true;
         }
-        if(!swap){
-          break;
-        }
+        
+     
       }
-      
+      if(!swap){
+          break;
+      }
     }
 
 
